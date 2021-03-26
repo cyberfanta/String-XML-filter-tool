5e74cf8e81d3882243588e1dca82a29546e84bfd
Julio César León <julioleon2004@gmail.com>
2021-03-26 02:29:09 -0400

    New UI design -- New Icons but Translate2.svg is not working properly

--- app/src/main/java/com/cyberfanta/stringsxmlfiltertool/MainActivity.java
+++ app/src/main/java/com/cyberfanta/stringsxmlfiltertool/MainActivity.java
@@ -76,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_2);
 
         // Device Metrics
         int deviceWidth;
